d9b5b85cc75d0bc9bcf7c2e0687d1076f798274d
poliwalker <pdm.1992@hotmail.com>
2022-10-11 17:24:31 -0300

    cambiamos estilo del scroll

---
+++ assets/arraylist.js
@@ -0,0 +1,11 @@
+let foods = [
+	{
+		id: 1,
+		nombre: 'La Mr Pit',
+		category: 'pizzas',
+		leyenda: 'Solo para expertos',
+		popular: true,
+		precio: 350,
+		img: '/assets/populares/Photo Menu.png',
+	},
+];
