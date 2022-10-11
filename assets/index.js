5031fe21010ccf50a48e552a6502b6377c592d81
sebastian <sebaberti.8@gmail.com>
2022-10-11 20:25:32 -0300

    elementos a llamar desde js y array de pizzas

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,38 @@
-const renderHTML = (food) => {
-	caja.innert.html = food.filter((food) => foods.id).join('');
-};
+//llamamos los elementos del carrito de compras
+const cart = document.getElementById('cart');
+const numberOrder = document.querySelector('.number-order');
+const sumarOrder = document.querySelector('.add-food-cart');
+const restarOrder = document.querySelector('.rest-food-cart');
+const cerrarCart = document.querySelector('.close-button');
+const btnComprar = document.querySelector('.buy-button');
+const subtotal = document.querySelector('.sub');
+const total = document.querySelector('.tot');
 
-const printHTML = (menu) => {
-	const { name, id, title, ingredients } = menu;
-	return `<div></div>`;
-};
+//llamamos los elementos de categorias
+const pizzas = document.querySelector('.cat-pizza');
+const hambuguesas = document.querySelector('.cat-burguer');
+const napapuki = document.querySelector('.cat-papas');
+const individuales = document.querySelector('.cat-individual');
+const wraps = document.querySelector('.cat-burrito');
+const tacos = document.querySelector('.cat-taco');
+const batidos = document.querySelector('.batido');
+
+const cardPopular = document.querySelector('.card-popular');
+const btnAdd = document.querySelector('.add');
+
+
+// const renderHTML = (food) => {
+// 	caja.innert.html = food.filter((food) => foods.id).join('');
+// };
+
+// const printHTML = (menu) => {
+// 	const { name, id, title, ingredients } = menu;
+// 	return `<div></div>`;
+// };
+
 
 const init = () => {
-	menuOptions.addEventListenner('click', renderHTML);
+	pizzas.addEventListenner('click', renderHtml);
 };
+
+init();
